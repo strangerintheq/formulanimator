3a1521d6fa77ace5451379060759b8aa6e519aa1
Konstantin Balashev <tripmannn@gmail.com>
2020-06-30 15:11:13 +0300

    Update formulanimator.js

--- formulanimator.js
+++ formulanimator.js
@@ -1,21 +1,36 @@
 function formulanimator(svg) {
   let script = document.createElement("script");
-  let elements = svg.querySelectorAll('*');
   svg.querySelectorAll('*').forEach(function(e) {
+      let count = e.getAttribute('count');
+      if (!count) return
+      for (let i = 1; i < count; i++){
+          let clone = e.cloneNode(true);
+          clone.setAttribute('index', i)
+          clone.querySelectorAll('*').forEach(function(child){
+              child.setAttribute('index', i) 
+          });
+          svg.append(clone)
+      }
+  })
+  let els = svg.querySelectorAll('*');
+  els.forEach(function(e) {
     e.funcs = {};
     for (let attr in e.dataset) {
       let f = 'f' + Math.random().toString(36).substring(2);
       e.funcs[attr] = f;
-      script.innerHTML += 'function '+f+'(t,el){return '+e.dataset[attr]+'}\n';
+      script.innerHTML += 'function '+f+'(t,i,e){\nreturn '+e.dataset[attr]+'\n}\n';
     }
   });
   document.body.append(script);
   requestAnimationFrame(redraw);
   
   function redraw(t) {
-    elements.forEach(function(element) {
-        for (let attr in element.dataset)
-          element.setAttribute(attr, window[element.funcs[attr]](t/1000, element));
+    t /= 1000;  
+    els.forEach(function(e) {
+        for (let a in e.dataset){
+          let i = e.getAttribute('index') || 0;
+          e.setAttribute(a, window[e.funcs[a]](t, i, e));
+        }
     });
     requestAnimationFrame(redraw);
   }   
