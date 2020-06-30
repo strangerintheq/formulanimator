6036b17ddc9c8adb1058b2c568a6ab53fb585ff9
Konstantin Balashev <tripmannn@gmail.com>
2020-06-30 16:07:46 +0300

    refactor, reduced fucntion count

--- formulanimator.js
+++ formulanimator.js
@@ -1,35 +1,38 @@
 function formulanimator(svg) {
-  let script = document.createElement("script");
-  svg.querySelectorAll('*').forEach(function(e) {
-      let count = e.getAttribute('count');
-      if (!count) return
-      for (let i = 1; i < count; i++){
-          let clone = e.cloneNode(true);
-          clone.setAttribute('index', i)
-          clone.querySelectorAll('*').forEach(function(child){
-              child.setAttribute('index', i) 
-          });
-          svg.append(clone)
+    
+  let script = document.createElement("script"), 
+      count = 0;
+    
+  svg.querySelectorAll('*').forEach(function(e) {    
+      for (let a in e.dataset) {
+          let f = 'f' + count++;
+          script.innerHTML += 'function '+f+'(t,i,e){\nreturn '+e.dataset[a]+'\n}\n';
+          e.dataset[a] = f;
       }
-  })
-  let els = svg.querySelectorAll('*');
-  els.forEach(function(e) {
-    e.funcs = {};
-    for (let attr in e.dataset) {
-      let f = 'f' + Math.random().toString(36).substring(2);
-      e.funcs[attr] = f;
-      script.innerHTML += 'function '+f+'(t,i,e){\nreturn '+e.dataset[attr]+'\n}\n';
-    }
   });
+    
+  svg.querySelectorAll('*[count]').forEach(function(e) {
+      for (let i = 1; i < e.getAttribute('count'); i++) {
+          let clone = e.cloneNode(true);
+          clone.setAttribute('index', i);
+          clone.querySelectorAll('*').forEach(function(child) {
+              child.setAttribute('index', i); 
+          });
+          svg.append(clone);
+      }
+  });  
+    
+  let els = svg.querySelectorAll('*');
   document.body.append(script);
   requestAnimationFrame(redraw);
   
   function redraw(t) {
     t /= 1000;  
     els.forEach(function(e) {
-        for (let a in e.dataset){
+        for (let a in e.dataset) {
           let i = e.getAttribute('index') || 0;
-          e.setAttribute(a, window[e.funcs[a]](t, i, e));
+          let f = e.dataset[a];  
+          f && e.setAttribute(a, window[f](t, i, e));
         }
     });
     requestAnimationFrame(redraw);
